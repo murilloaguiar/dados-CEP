f620c58e066a84fb66a57a663b1f5b71a9e4830b
Murillo Aguiar <aguiar_murillo@outlook.com>
2021-11-05 15:48:16 -0300

    funcionalidade básica
    mostrando os dados corretamente a partir de um CEP que seja válido.  Sem verificação de validade do CEP ainda. Template básico.

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 const button = document.querySelector('#submit')
 
-button.addEventListener('click', searchCEP)
+const insertDataCEP = data =>{
+    document.querySelector('#street').innerHTML = data.logradouro
+    document.querySelector('#city').innerHTML = data.localidade
+    document.querySelector('#district').innerHTML = data.bairro
+    document.querySelector('#state').innerHTML = data.uf
+    document.querySelector('#ddd').innerHTML = data.ddd
 
-const searchCEP = ()=>{
-    let cep = document.querySelector('input').value
-
-    
-    fetch('https://viacep.com.br/ws/'+cep+'/json/unicode/')
-        .then(response =>{
-            
-            
-            console.log(response.json())
-        })
 }
+
+const searchCEP = async ()=>{
+    let cep = document.querySelector('#search').value
+    let date = new Date
+    let ibge = ""
+    console.log(cep)
+   
+    await fetch(`https://viacep.com.br/ws/${cep}/json/unicode/`)
+        .then(response =>response.json())
+        .then(data=>{
+            console.log(data)
+            insertDataCEP(data)
+            ibge = data.ibge
+        })
+    
+    await fetch(`https://servicodados.ibge.gov.br/api/v3/agregados/6579/periodos/${date.getFullYear()}/variaveis/9324?localidades=N6[${ibge}]`)
+            .then(response => response.json())
+            .then(data => {
+                document.querySelector('#population').innerHTML = `${data[0].resultados[0].series[0].serie[date.getFullYear()]} pessoas`
+            })
+
+}
+
+button.addEventListener('click', searchCEP)
