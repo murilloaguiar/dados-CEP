8f093489d5ee1367b4307adbbc85afbe8fcb5272
Murillo Aguiar <aguiarmurillo1@gmail.com>
2022-02-25 11:34:44 -0300

    Removendo códigos desnecessários

--- assets/js/script.js
+++ assets/js/script.js
@@ -124,33 +124,15 @@ const insertAgricultura = (unidade, variavel, dados) =>{
       div_card_principal.appendChild(div_card)
 
       div_agricultura.appendChild(div_card_principal)
-   
-        /*let categoria = dados[key].classificacoes[0].categoria
-        for (const key2 in categoria) {
-            let h5 = document.createElement('h5')
-            h5.innerHTML = `${categoria[key2]}`
-            document.querySelector("#agricultura").appendChild(h5)
-            
-        }
-        
-        let anos = dados[key].series[0].serie
-        for (const key3 in anos) {
-            let p = document.createElement('p')
-            p.innerHTML = `${key3}: ${anos[key3]}`
-            document.querySelector("#agricultura").appendChild(p)
-            
-        }*/
-    }
+
+   }
 }
 
 const searchCEP = async ()=>{
    document.querySelector('#main').classList.remove('d-none')
 
-
    let cep = document.querySelector('#search').value
    let ibge = ""
-   let microrregiao = ""
-   
 
    /*dados do cep*/ 
    await fetch(`https://viacep.com.br/ws/${cep}/json/unicode/`)
@@ -160,36 +142,15 @@ const searchCEP = async ()=>{
          ibge = data.ibge
       })
    
-
-   /*recuperando a microrregiao para obter dados agricolas*/
-   await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/municipios/${ibge}`)
-      .then(response => response.json())
-      .then(data => {
-         microrregiao = data.microrregiao.id
-   
-      })
-
    /*dados agricolas*/    
-   await fetch(`https://servicodados.ibge.gov.br/api/v3/agregados/1613/periodos/-3/variaveis/2313?localidades=N6[${ibge}]&classificacao=82[2717,45981,2719,2720,2722,2723,40473,2725,2728,2731,2732,2733,2734,2735,2736,2737,2738,90001,2741,2742,2745,2748]`)
+   await fetch(`https://servicodados.ibge.gov.br/api/v3/agregados/1613/periodos/-4/variaveis/2313?localidades=N6[${ibge}]&classificacao=82[2717,45981,2719,2720,2722,2723,40473,2725,2728,2731,2732,2733,2734,2735,2736,2737,2738,90001,2741,2742,2745,2748]`)
       .then(response => response.json())
       .then(data => {
-
-         let unidade = data[0].unidade
-         
-         let variavel = data[0].variavel
-         
 
          let dados = data[0].resultados
          console.log(dados)
 
          insertAgricultura(unidade, variavel, dados)
-
-         /*console.log(data[0].resultados[0].classificacoes[0].categoria)
-
-         for (const key in dados) {
-               console.log(data[0].resultados[key].classificacoes[0].categoria)
-               console.log(data[0].resultados[key].series[0].serie)
-         }*/
 
       })
 
